2053f7d3720123e6915f4fd3222c7b01fe4f8b6a
Seiya-Umemoto <seiya.u77@gmail.com>
2020-05-29 11:17:28 +0900

    revised if statement in age

--- CalAdm.java
+++ CalAdm.java
@@ -8,11 +8,11 @@
 public class CalAdm
 {
     int fee;
-    public double calAdm(int age) {
-        if (age == 4) {fee = 0;}
+    public int calAdm(int age) {
+        if (0 < age == 4) {fee = 0;}
         else if (age < 4) {fee = 400;}
         else if (age <7) {fee = 900;}
-        else {fee = 2000;}
+        else if (16 < age) {fee = 2000;}
         return fee;
     }
 }
