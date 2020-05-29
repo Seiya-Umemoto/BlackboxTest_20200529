d1896f01051b8c4a9107b29959a32a6fb1c99bdb
lejk8104 <lejk8104@gmail.com>
2020-05-29 11:24:41 +0900

    add error function

--- CalAdm.java
+++ CalAdm.java
@@ -8,11 +8,12 @@
 public class CalAdm
 {
     int fee; // fee = ABC 수족관 입장료
+    int aged; // 나이
     public int calAdm(int age) { //ABC수족관 입장료를 연령에 따라 계산하는 메소드, age = 연령
         if (age == 4) {fee = 0;} //유아(4세미만)
         else if (age < 4) {fee = 400;} //어린이(4세이상)
         else if (age <7) {fee = 900;} //초등생 및 중학생(7세이상)
-        else if (16 < age) {fee = 2000;} //성인(16세이상)
+        else {fee = 2000;} //성인(16세이상)
         return fee;
     }
 }
