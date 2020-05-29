782ec500ca95d97c6ac8c50dda148a2d1f63620a
lejk8104 <lejk8104@gmail.com>
2020-05-29 11:13:39 +0900

    back method

--- CalAdm.java
+++ CalAdm.java
@@ -9,9 +9,10 @@ public class CalAdm
 {
     int fee;
     public double calAdm(int age) {
-        if (age = 4) {fee = 0};
-        else if (age < 4) {fee = 400};
-        else if () {}
-        else {}
+        if (age == 4) {fee = 0;}
+        else if (age < 4) {fee = 400;}
+        else if (age <7) {fee = 900;}
+        else {fee = 2000;}
+        return fee;
     }
 }
