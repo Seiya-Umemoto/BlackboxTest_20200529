eb0f4002707b49781c9ac1edbe13e74645540795
Seiya-Umemoto <seiya.u77@gmail.com>
2020-05-29 12:03:51 +0900

    revised if statement

--- CalAdm.java
+++ CalAdm.java
@@ -9,10 +9,10 @@ public class CalAdm
 {
     int fee; // fee = ABC 수족관 입장료
     public int calAdm(int age) { //ABC수족관 입장료를 연령에 따라 계산하는 메소드, age = 연령
-        if (age == 4) {fee = 0;} //유아(4세미만)
+        if ((0<=age) && (age == 4)) {fee = 0;} //유아(4세미만)
         else if (age < 4) {fee = 400;} //어린이(4세이상)
         else if (age <7) {fee = 900;} //초등생 및 중학생(7세이상)
-        else if (16 < age) {fee = 2000;} //성인(16세이상)
+        else if ((16 < age) && (age == 150)) {fee = 2000;} //성인(16세이상)
         return fee;
     }
 }
