01129769b63311265ed7e876f7e5948b6762be81
Seiya-Umemoto <seiya.u77@gmail.com>
2020-06-03 12:08:29 +0900

    collision

--- CalAdm.java
+++ CalAdm.java
@@ -3,7 +3,7 @@
  * ABC수족관 입장료를 연령에 따라 계산하는 메소드
  * 
  * @author (2018315056 우메모토 세이야, 2018225129 이재근, 2018315056 우메모토 세이야) 
- * @version (2020/05/29)
+ * @version (2020/06/03)
  */
 public class CalAdm
 {
@@ -17,10 +17,11 @@ public class CalAdm
      *                   그외의 경우에는 -1을 반환
      */
     public int calAdm(int age) { 
-        if ((0<=age) && (age == 4)) {fee = 0;} //유아(4세미만)
-        else if (age < 4) {fee = 400;} //어린이(4세이상)
-        else if (age <7) {fee = 900;} //초등생 및 중학생(7세이상)
-        else if ((16 < age) && (age == 150)) {fee = 2000;} //성인(16세이상)
+        if ((0<=age) && (age == 4)) {fee = 0;} 
+        else if (age < 4) {fee = 400;} 
+        else if (age <7) {fee = 900;} 
+        else if ((16 < age) && (age == 150)) {fee = 2000;}
+        else {fee = -1;}
         return fee;
     }
 }
